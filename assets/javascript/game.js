04589fac3fcea33a176e629c429d050b9b5aef2b
Mayra Jaimes <mtj26@georgetown.edu>
2017-12-14 22:56:03 -0600

    changing and adding functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -49,68 +49,65 @@ $(document).ready(function() {
 			$('#villain' + i + ' .HP').html(villains['villain' + i].hp);
 	}
 
-	function heroDefeated(){
+
+	function defeatedHero(){
 		gameOver = true;
 		attackButton.css("visibility", "hidden");
 		resetButton.css("visibility", "visible");
 		villainPickedResultsP.html(`${chosenVillain.name} won!`); 		
-	 	heroPickedResultsP.html(`${chosenHero.name} was defeated. You lost! Click "restart" to play again!`); 
- 	}
-
-	function resetGame () {
-		resetButton.on("click", function() {
-			isHeroChosen = false;
-			isVillainChosen = false;
-			villainsToFight = 4;
-			gameOver = false;
-			heroes = {};
-			villains = {};
-
-			Character = function(name, ap, isHero) {
-				this.name = name;
-				this.ap = ap;
-				this.hp = Math.floor(Math.random()*200) + 100;
-				this.isHero = isHero || false;
-			}
-			
-			heroesList = [
-			{name: 'Captain America', ap: 20, isHero: true}, 
-			{name: 'Spiderman', ap: 15, isHero: true}, 
-			{name: 'Thor', ap: 5, isHero: true}, 
-			{name: 'Batman', ap: 10, isHero: true}];
-			
-			villList = [
-			{name: 'Red Skull', ap: 20}, 
-			{name: 'Joker', ap: 15}, 
-			{name: 'Loki', ap: 5}, 
-			{name: 'Green Goblin', ap: 10}];
-
-			for (i=1; i<heroesList.length+1; i++) {
-				heroes['hero' + i] = new Character(heroesList[i-1].name, heroesList[i-1].ap, heroesList[i-1].isHero);
-				$('#hero' + i + ' .HP').html(heroes['hero' + i].hp);
-			}
-
-			for (i=1; i<villList.length+1; i++) {
-				villains['villain' + i] = new Character(villList[i-1].name, villList[i-1].ap);
-				$('#villain' + i + ' .HP').html(villains['villain' + i].hp);
-			}
-
-			$(".villainPicked").empty();
-			$(".heroPicked").empty();
-			$(".villainPickedSubheading").empty();
-			$(".heroPickedSubheading").empty();
-			heroPickedResultsP.empty(); 
-			villainPickedResultsP.empty();
-			heroesContent.removeClass("heroesRemaining");
-			$(".villain").removeClass('defeated');
-			villainsContent.removeClass("villainsRemaining");
-			resetButton.css("visibility", "hidden");
-		});
-
+		heroPickedResultsP.html(`${chosenHero.name} was defeated. You lost! Click "restart" to play again!`);
 	}
 
-	function initializeGame() {
+	function resetGame(){
+		isHeroChosen = false;
+		isVillainChosen = false;
+		villainsToFight = 4;
+		gameOver = false;
+		heroes = {};
+		villains = {};
 
+		Character = function(name, ap, isHero) {
+			this.name = name;
+			this.ap = ap;
+			this.hp = Math.floor(Math.random()*200) + 100;
+			this.isHero = isHero || false;
+		}
+		
+		heroesList = [
+		{name: 'Captain America', ap: 20, isHero: true}, 
+		{name: 'Spiderman', ap: 15, isHero: true}, 
+		{name: 'Thor', ap: 5, isHero: true}, 
+		{name: 'Batman', ap: 10, isHero: true}];
+		
+		villList = [
+		{name: 'Red Skull', ap: 20}, 
+		{name: 'Joker', ap: 15}, 
+		{name: 'Loki', ap: 5}, 
+		{name: 'Green Goblin', ap: 10}];
+
+		for (i=1; i<heroesList.length+1; i++) {
+			heroes['hero' + i] = new Character(heroesList[i-1].name, heroesList[i-1].ap, heroesList[i-1].isHero);
+			$('#hero' + i + ' .HP').html(heroes['hero' + i].hp);
+		}
+
+		for (i=1; i<villList.length+1; i++) {
+			villains['villain' + i] = new Character(villList[i-1].name, villList[i-1].ap);
+			$('#villain' + i + ' .HP').html(villains['villain' + i].hp);
+		}
+
+		$(".villainPicked").empty();
+		$(".heroPicked").empty();
+		$(".villainPickedSubheading").empty();
+		$(".heroPickedSubheading").empty();
+		heroPickedResultsP.empty(); 
+		villainPickedResultsP.empty();
+		heroesContent.removeClass("heroesRemaining");
+		$(".villain").removeClass('defeated');
+		villainsContent.removeClass("villainsRemaining");
+		resetButton.css("visibility", "hidden");
+	};
+
+	function initializeGame() {
 	    $(".hero").on("click", function() {
 	    	if (!isHeroChosen){
 	    		var heroId = $(this).attr('id');
@@ -124,10 +121,8 @@ $(document).ready(function() {
 		});
 
 //Click Villain
-
 		$(".villain").on("click", function() {
 			currentVillain = $(this);
-
 			if (!isVillainChosen && isHeroChosen) {
 				villainsToFight --;
 				var villainId = $(this).attr('id');
@@ -142,8 +137,7 @@ $(document).ready(function() {
 		});
 
 //Attack button
-		
-		$(".attackButton").on("click", function() {
+		attackButton.on("click", function() {
 			if (isVillainChosen && isHeroChosen && chosenHero.hp > 0 && chosenVillain.hp > 0 && villainsToFight >= 0 ) {
 				chosenVillain.hp -= chosenHero.ap;
 				chosenHero.hp -= chosenVillain.ap;
@@ -159,12 +153,10 @@ $(document).ready(function() {
 				villainPickedResultsP.html(`${chosenVillain.name} lost.`); 
 				heroPickedResultsP.html(`You won this round against ${chosenVillain.name}. Pick a new villain to fight!`); 		
 				villainsContent.removeClass("villainsRemaining");
-
 		 		heroPickedResultsP.empty(); 
 				$(".villainPicked").empty();
 				$(".villainPickedSubheading").empty();
 				villainPickedResultsP.empty();
-
 				currentVillain.addClass('defeated');
 				isVillainChosen = false;
 			}
@@ -178,12 +170,13 @@ $(document).ready(function() {
 			}
 
 			if (chosenHero.hp <= 0) {
-				heroDefeated ();
-			}
+				defeatedHero(); 
+		 	}
+		});
 
-			if (gameOver = true) {
-				resetGame();
-			}
+//Reset Button
+		resetButton.on("click", function() {
+			resetGame();	
 		});
 	};	
 
